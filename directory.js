28720a2d0cb6bcc80d9eb7bd9733832ecfa20cbd
Mário Pereira <mario.pedro@niceoutput.com>
2015-09-08 23:31:41 +0100

    add a form event

--- directory.js
+++ directory.js
@@ -7,10 +7,16 @@ angular.module('directoryApp', [])
 	.controller('directoryController', function($scope){
 		$scope.list = [
 			
-			{name:'Monteiro', role: 'Full Stack Unicorn'}, 
-			{name:'Gibão', role:'Core Magician'}, 
-			{name:'Aragão', role:'Translations Pimp'}, 
-			{name:'Ribeiro', role:'Deploy Arquitect'}
+			{name:'Monteiro', role: 'Full Stack Unicorn', age: 20}, 
+			{name:'Gibão', role:'Core Magician', age: 25}, 
+			{name:'Aragão', role:'Translations Pimp', age: 25}, 
+			{name:'Ribeiro', role:'Deploy Arquitect', age: 23}
 			
-			]
+			];
+
+			$scope.addPerson = function() {
+				$scope.list.push({name: $scope.name, role: $scope.role});
+				$scope.name = '';
+				$scope.role = '';
+			};
 	});
