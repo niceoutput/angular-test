0a3f2d37f1a7ec609e5f780502b4e484f26295b6
Mário Pereira <mario.pedro@niceoutput.com>
2015-09-17 00:20:04 +0100

    adding ng-animations for the states

--- directory.js
+++ directory.js
@@ -3,7 +3,8 @@
 *
 * Description
 */
-angular.module('directoryApp', [])
+angular.module('directoryApp', ['ngAnimate'])
+	
 	.controller('directoryController', function(){
 
 		var dirList = this;
@@ -14,7 +15,7 @@ angular.module('directoryApp', [])
 			
 			{name:'Monteiro', role: 'Full Stack Unicorn', age: 20, img: 'http://api.adorable.io/avatars/200/abott@adorable.io.png'}, 
 			{name:'Gibão', role:'Core Magician', age: 25, img: 'http://api.adorable.io/avatars/200/swa@adorable.io.png'}, 
-			{name:'Aragão', role:'Translations Pimp', age: 25, img: 'http://api.adorable.io/avatars/200/abott@adorable.io.png'}, 
+			{name:'Aragão', role:'Translations Pimp', age: 25, img: 'http://api.adorable.io/avatars/200/der@adorable.io.png'}, 
 			{name:'Ribeiro', role:'Deploy Arquitect', age: 23, img: 'http://api.adorable.io/avatars/200/abott@adorable.io.png'}
 			
 			];
